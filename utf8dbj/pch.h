da313f953800988b2f59503d290382d0912f089e
dbjdbj <dbj@dbj.org>
2018-12-04 23:17:23 +0100

    font name and is it truetype, info is shown...

--- utf8dbj/pch.h
+++ utf8dbj/pch.h
@@ -1,7 +1,9 @@
 
 #ifndef PCH_H
 #define PCH_H
-
+#define NOMINMAX
+#define STRICT
+#include <windows.h>
 #include <array>
 #include <vector>
 #include <string>
@@ -10,6 +12,8 @@
 #include <math.h>
 #include <fcntl.h>  
 #include <io.h> 
+#include <stdio.h> 
+#include <wchar.h> 
 #ifndef _HAS_CXX17
 #	error C++17 please ...
 #endif
@@ -18,5 +22,23 @@
 #define ST_1(x) ST_2(x)
 #define ST(x) ST_1(x)
 #define TU(x) printf("\n",ST(x));
+
+// first a good decades old VERIFY macro
+namespace dbj {
+	[[noreturn]]
+	inline void terror
+	(const char * msg_, const char * file_, const int line_)
+	{
+		assert(msg_);
+		assert(file_);
+		assert(line_);
+		::fwprintf(stderr, L"\n\ndbj++ ERROR:%S\n%S (%d)", msg_, file_, line_);
+		exit(EXIT_FAILURE);
+	}
+
+#define DBJ_VERIFY_(x, file, line ) if (false == (x) ) ::dbj::terror( #x ", failed", file, line )
+
+#define DBJ_VERIFY(x) DBJ_VERIFY_(x,__FILE__,__LINE__)
+} // dbj
 
 #endif //PCH_H
--- utf8dbj/utf8dbj.cpp
+++ utf8dbj/utf8dbj.cpp
@@ -1,8 +1,37 @@
-﻿#include "pch.h"
+﻿/*
+Copyright 2018 by DBJ.Systems Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http ://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "pch.h"
 using namespace std;
 using namespace std::string_view_literals;
 
-// NOTE: of course be sure your project is UNICODE built
+	// return font name and true/false 
+	// depending on is it true type or not
+static auto console_font_info()
+{
+	HANDLE output_ = GetStdHandle(STD_OUTPUT_HANDLE);
+	DBJ_VERIFY(output_ != INVALID_HANDLE_VALUE);
+	CONSOLE_FONT_INFOEX info{ sizeof(CONSOLE_FONT_INFOEX) };
+	DBJ_VERIFY(0 != GetCurrentConsoleFontEx(output_, false, &info));
+
+	return make_pair(
+		wstring( info.FaceName ),
+		(info.FontFamily & TMPF_TRUETYPE) == TMPF_TRUETYPE
+    );
+}
+
 
 int main()
 {
@@ -20,7 +49,6 @@ int main()
 	::system("@echo.");
 	::system("@echo If specimen bellow is not readable, please change the console");
 	::system("@echo font to the one that will make the specimen fully readable");
-	::system("@echo Please do this change now");
 	::system("@echo Repeat untill you guessed the right font");
 	// start this app
 	//
@@ -28,41 +56,20 @@ int main()
 	constexpr auto specimen =
 		L"\x043a\x043e\x0448\x043a\x0430 \x65e5\x672c\x56fd";
 
-	if ( int result = _setmode(_fileno(stdout), _O_U8TEXT); result == -1) {
-		perror("Cannot set mode");
-		exit(EXIT_FAILURE);
-	}
+	DBJ_VERIFY(_setmode(_fileno(stdout), _O_U8TEXT) != -1);
 	// should display: кошка 日本
 	::wprintf(L"\n\n\tspecimen:\t%s\n\n", specimen);
 
-	// NOTE: can not use printf()!
-	// for any mode the following crashes 
-	// the UCRT (aka Universal CRT)
-	// crashes on
-	// _O_U16TEXT	-- crash
-	// _O_U8TEXT	-- crash
-	// _O_WTEXT		-- crash
-	// _O_TEXT		-- no crash but hardly any output
-	// _O_BINARY	-- no crash but hardly any output
-	//
-	// ::printf("\nprintf()\n\tspecimen: %S\n", specimen);
+	auto [font_name, font_is_tt_ ] = console_font_info();
+	const wchar_t * not_ = (font_is_tt_ ? L"" : L"NOT");
+	::wprintf(L"\n\n\tCurrent font name is '%s', this is %s true type font.\n\n", 
+		font_name.c_str(), not_);
 
 	::system("@echo.");
 	::system("@echo If you see the specimen fully and clearly, you have found the right font");
 	::system("@echo Each time you need to display UTF-8 text properly in the console on this desktop");
-	::system("@echo You will have to use this font and execute 'chcp 65001', before.");
+	::system("@echo You will have to use this font and execute 'chcp 65001', before that.");
 	::system("@echo.");
 	::system("@pause");
 	return 1;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
