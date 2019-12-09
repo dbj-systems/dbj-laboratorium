2e5a6585208b1b4583604eb532f17892bdd5ee78
dbjdbj <dbj@dbj.org>
2019-12-09 23:58:52 +0100

    CC BY SA 4.0

--- anything_but/main.cpp
+++ anything_but/main.cpp
@@ -1,19 +1,9 @@
 /*
-    Type Handle to Avoid Implicit Conversions in standard C++
+  Type Handle to Avoid Implicit Conversions in standard C++
 
-  Copyright 2019 Dušan B. Jovanović (dbj@dbj.org)
+  Copyright 2019/2020 Dušan B. Jovanović (dbj@dbj.org)
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-	   http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+  Please see the LICENSE file
 */
 #include <exception>
 #include <cstdio>
